5e1a8d1470721ace0e3ceee7179c8e5f24a4e8d4
LaxmiRathore <rathorelaxmi.rathore@gmail.com>
2017-10-20 19:21:12 +0530

    this is my fifth commit

--- src/com/emids/javaclass/Calculator.java
+++ src/com/emids/javaclass/Calculator.java
@@ -1,21 +1,28 @@
 package com.emids.javaclass;
 
 public class Calculator {
-public static int  add(String numbers)
-	{
-		String[] numbersArray=numbers.split(",");
-		
-		if(numbersArray.length==0)
+	public static int add(String numbers) {
+		 String delimeter = ",|\n|;";
+
+		String[] numbersArray = numbers.split(delimeter);
+
+	
+		if (numbersArray.length == 0)
 			return 0;
-		else
-		{
-			int sum=0;
+		int sum = 0;
 		for (String number : numbersArray) {
-			int digit=Integer.parseInt(number);
-					sum+=digit;
+			char[] arr = number.toCharArray();
+			String num = "";
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] >= '0' && arr[i] <= '9')
+				{
+					num = num + arr[i];
+				}
+			}
+			int digit = Integer.parseInt(num);
+			sum = sum + digit;
 		}
+		
 		return sum;
 		}
-	}
-
 }
